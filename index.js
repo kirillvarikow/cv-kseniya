33cdd5c7430e1b8a63d65ec00b7c27c14411ec24
kirillvarikow <cyril.varikov@gmail.com>
2022-10-16 17:10:26 +0200

    auto-generation

---
+++ index.js
@@ -0,0 +1,19 @@
+const puppeteer = require('puppeteer');
+const fileUrl = require('file-url');
+
+(async() => {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.goto(fileUrl('index.html'));
+	await page.pdf({
+		path: 'test.pdf',
+		format: 'A4',
+		margin: {
+			top: "20px",
+			left: "20px",
+			right: "20px",
+			bottom: "20px"
+		}
+	});
+	await browser.close();
+})();
